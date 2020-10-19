efbf433a029b0358c5fc1094183d8e16c6155275
Kateryna Chovhan <chovganek@gmail.com>
2020-10-20 00:02:29 +0300

    feat: add result of brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const config = bracketsConfig
+  .reduce((res, [bracketOpen, bracketClose])=>
+   ({ ...res, [bracketOpen]: bracketClose }), {});
+
+str = str.split('');
+let stack = [];
+let bool = false;
+
+for (bracket of str) {
+  if (stack.includes(config[bracket])) {
+    bool = true;
+    // console.log(bool);
+  }
+  if(config[bracket] && !bool) {
+    stack.push(bracket);
+    // console.log(stack);
+  } else {
+    if (bracket === config[stack.pop()]) {
+      bool = false;
+    }
+    else { 
+      return false;
+    }
+    }
+  }
+  console.log(stack);
+  if (stack.length !== 0) return false;
+  return true;
 }
